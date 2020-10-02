03924144972b3ac857a49b5404497529d3d022dd
maiconandsilva <64911189+maiconandsilva@users.noreply.github.com>
2020-10-02 13:54:14 -0300

    Modifica Configuracoes para Adicionar Base de Dados isolada

---
+++ models/isolated/customer_personal_info.py
@@ -0,0 +1,6 @@
+from app import db
+
+class CustomerPersonalInfo(db.Model):
+    __bind_key__ = 'db_isolated'
+
+
--- settings.py
+++ settings.py
@@ -5,4 +5,7 @@ class Settings:
     TESTING = True
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URI']
+    SQLALCHEMY_BINDS = {
+        'db_isolated': os.environ['ISOLATED_DATABASE_URI']
+    }
     SQLALCHEMY_TRACK_MODIFICATIONS = False
