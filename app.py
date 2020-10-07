e854600e17826c3d5836e82d6f7d38e8ffbaa772
maiconandsilva <64911189+maiconandsilva@users.noreply.github.com>
2020-10-06 22:27:34 -0300

    Adiciona resposta para exibição

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ def account_delete():
         "message": "An error ocurred",
         "success": False,
     }
+    customerid = None
 
     if request.method == 'POST':
 
@@ -36,9 +37,38 @@ def account_delete():
         db.session.add(customer)
         db.session.commit()
 
-        data.update(message="Deleted", success=True)
-        return jsonify(data), 200
-    return jsonify(data), 400
+        # data.update(message="Deleted", success=True)
+
+        customer = Customers.query.get(customerid)
+    else:
+        return jsonify(data), 404
+    return f"""
+        <h1>Customer</h1>
+        <ul>
+            <li>Email: {customer.email}</li>
+            <li>Genero: {customer.gender}</li>
+            <li>Customerid: {customer.customerid}</li>
+            <li>Firstname: {customer.firstname}</li>
+            <li>Lastname: {customer.lastname}</li>
+            <li>Address1: {customer.address1}</li>
+            <li>Address2: {customer.address2}</li>
+            <li>City: {customer.city}</li>
+            <li>State: {customer.state}</li>
+            <li>Zip: {customer.zip}</li>
+            <li>Country: {customer.country}</li>
+            <li>Region: {customer.region}</li>
+            <li>Email: {customer.email}</li>
+            <li>Phone: {customer.phone}</li>
+            <li>Creditcardtype: {customer.creditcardtype}</li>
+            <li>Creditcard: {customer.creditcard}</li>
+            <li>Creditcardexpiration: {customer.creditcardexpiration}</li>
+            <li>Username: {customer.username}</li>
+            <li>Password: {customer.password}</li>
+            <li>Age: {customer.age}</li>
+            <li>Income: {customer.income}</li>
+            <li>Gender: {customer.gender}</li>
+        </ul>
+    """
 
 
 if __name__ == '__main__':
