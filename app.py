2a01414482101dfa139d08c190d088121cf231b7
maiconandsilva <64911189+maiconandsilva@users.noreply.github.com>
2020-10-27 16:45:11 -0300

    Muda inicializacao do Flask e SQLAlchemy para app.py

--- app.py
+++ app.py
@@ -1,78 +1,12 @@
-from flask import Flask, request
-from flask.json import jsonify
-from server import app, db
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import import_string
 
-from models import Customers
+import os
 
-@app.route('/account-delete', methods=['GET', 'POST'])
-def account_delete():
-    data = {
-        "message": "An error ocurred",
-        "success": False,
-    }
-    customerid = None
+app = Flask(__name__)
 
-    if request.method == 'POST':
+cfg = import_string(os.environ.get('FLASK_APPSETTINGS'))()
+app.config.from_object(cfg)
 
-        try:
-            customerid = int(request.form.get('customerid'))
-        except ValueError:
-            return jsonify(data), 400
-
-        customer = Customers.query.get(customerid)
-        
-        if customer is None:
-            return jsonify(data), 404
-
-        customer.firstname = None
-        customer.lastname = None
-        customer.address1 = None
-        customer.creditcardtype = None
-        customer.creditcard = None
-        customer.creditcardexpiration = None
-        customer.username = None
-        customer.password = None
-        customer.deleted = 'Y'
-
-        db.session.add(customer)
-        db.session.commit()
-
-        # data.update(message="Deleted", success=True)
-
-        customer = Customers.query.get(customerid)
-    else:
-        return jsonify(data), 404
-    return f"""
-        <h1>Customer</h1>
-        <ul>
-            <li>Email: {customer.email}</li>
-            <li>Genero: {customer.gender}</li>
-            <li>Customerid: {customer.customerid}</li>
-            <li>Firstname: {customer.firstname}</li>
-            <li>Lastname: {customer.lastname}</li>
-            <li>Address1: {customer.address1}</li>
-            <li>Address2: {customer.address2}</li>
-            <li>City: {customer.city}</li>
-            <li>State: {customer.state}</li>
-            <li>Zip: {customer.zip}</li>
-            <li>Country: {customer.country}</li>
-            <li>Region: {customer.region}</li>
-            <li>Email: {customer.email}</li>
-            <li>Phone: {customer.phone}</li>
-            <li>Creditcardtype: {customer.creditcardtype}</li>
-            <li>Creditcard: {customer.creditcard}</li>
-            <li>Creditcardexpiration: {customer.creditcardexpiration}</li>
-            <li>Username: {customer.username}</li>
-            <li>Password: {customer.password}</li>
-            <li>Age: {customer.age}</li>
-            <li>Income: {customer.income}</li>
-            <li>Gender: {customer.gender}</li>
-        </ul>
-    """
-
-
-if __name__ == '__main__':
-    from debugger import initialize_flask_server_debugger_if_needed
-    
-    initialize_flask_server_debugger_if_needed()
-    app.run(host='0.0.0.0', port=5000)
+db = SQLAlchemy(app)
