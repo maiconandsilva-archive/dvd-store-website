e359ffe3d26a97a933089088f9d7f3eeceed7724
maiconandsilva <64911189+maiconandsilva@users.noreply.github.com>
2020-10-27 18:32:25 -0300

    Add functionality to anonynimize customer

--- models/customers.py
+++ models/customers.py
@@ -1,4 +1,10 @@
-from server import db
+from datetime import datetime
+
+from flask.globals import request
+from app import db
+
+import sqlalchemy_utils as su
+
 
 customerhistory = db.Table('cust_hist',
     db.Column('customerid', db.Integer,
@@ -12,29 +18,61 @@ customerhistory = db.Table('cust_hist',
 class Customers(db.Model):
     __tablename__ = 'customers'
 
+    MALE = 'M'
+    FEMALE = 'F'
+    GENDERS = [
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+    ]
+    # TODO: Add unique constraint to columns
     customerid = db.Column(db.Integer, primary_key=True)
-    firstname = db.Column(db.String(50))
-    lastname = db.Column(db.String(50))
-    address1 = db.Column(db.String(50))
-    address2 = db.Column(db.String(50))
-    city = db.Column(db.String(50))
-    state = db.Column(db.String(50))
-    zip = db.Column(db.Integer)
-    country = db.Column(db.String(50))
+    firstname = db.Column(db.String(50), info={'anonymize': True})
+    lastname = db.Column(db.String(50), info={'anonymize': True})
+    address1 = db.Column(db.String(50), info={'anonymize': True})
+    address2 = db.Column(db.String(50), info={'anonymize': True})
+    city = db.Column(db.String(50), info={'anonymize': True})
+    state = db.Column(db.String(50), info={'anonymize': True})
+    zip = db.Column(db.Integer, info={'anonymize': True})
+    country = db.Column(su.CountryType, info={'anonymize': True})
     region = db.Column(db.Integer)
-    email = db.Column(db.String(50))
-    phone = db.Column(db.String(50))
-    creditcardtype = db.Column(db.Integer)
-    creditcard = db.Column(db.String(50))
-    creditcardexpiration = db.Column(db.String(50))
-    username = db.Column(db.String(50))
-    password = db.Column(db.String(50))
-    age = db.Column(db.Integer)
-    income = db.Column(db.Integer)
-    gender = db.Column(db.String(1))
-    deleted = db.Column(db.String(1), nullable=False, default='N')
+    email = db.Column(su.EmailType(50))
+    phone = db.Column(su.PhoneNumberType(max_length=50),
+        info={'anonymize': True})
+    creditcardtype = db.Column(db.Integer, info={'anonymize': True})
+    creditcard = db.Column(db.String(50), info={'anonymize': True})
+    creditcardexpiration = db.Column(db.String(50),
+        info={'anonymize': True})
+    username = db.Column(db.String(50), info={'anonymize': True})
+    password = db.Column(su.PasswordType(schemes=['pbkdf2_sha512']),
+        info={'anonymize': True})
+    age = db.Column(db.Integer, info={'anonymize': True})
+    income = db.Column(db.Integer, info={'anonymize': True})
+    gender = db.Column(su.ChoiceType(GENDERS,
+        impl=db.String(1)), info={'anonymize': True})
+    _deleted_at = db.Column('deleted_at', db.DateTime)
     shopping_history = db.relationship('Orders', secondary='cust_hist')
-    products_bought = db.relationship('Products', secondary='cust_hist')
+
+    @property
+    def is_active(self):
+        return self._deleted_at is None
+  
+    @classmethod
+    def from_form(cls, form):
+        """Retorna um novo Customer a partir de um form"""
+        customer = cls()
+        form.populate_obj(customer)
+        return customer
+
+    def anonymized(self):
+        """Anonimiza informacoes que identificam uma pessoa"""
+        # TODO: Change anonymization process to
+        # TODO: Atualizar forma de aplicar data
+        self._deleted = datetime.now()
+        for column in self.__table__.columns:
+            if column.info.get('anonymize'):
+                setattr(self, column.name, None)
+        return self
+
 
 class Orders(db.Model):
     """Tabela de pedidos"""
@@ -49,6 +87,8 @@ class Orders(db.Model):
     tax = db.Column(db.Numeric(precision=12, scale=2), nullable=False)
     totalamount  = db.Column(db.Numeric(precision=12, scale=2), nullable=False)
     customer = db.relationship('Customers')
+    products = db.relationship('Products', secondary='cust_hist')
+
 
 class OrderLines(db.Model):
     """Tabela com os produtos por pedido"""
