930f88be84904abe4f3bb7482066d15f0b8d5c3a
Maicon Mauricio <64911189+maiconandsilva@users.noreply.github.com>
2020-11-01 19:17:00 -0300

    Add isolated database model with encrypted columns

--- models/isolated/customer_personal_info.py
+++ models/isolated/customer_personal_info.py
@@ -1,5 +1,43 @@
-from app import db
+from flask.globals import session
+from sqlalchemy_utils.types.encrypted.encrypted_type import AesEngine, FernetEngine
+from sqlalchemy_utils import EncryptedType
+from cryptography.fernet import Fernet
 
-class CustomerPersonalInfo(db.Model):
-    __bind_key__ = 'db_isolated'
-    
+from app import db
+from models.customers import Customers
+
+
+def _get_customer_personal_info(model, ColumnType, engine, key):
+
+    AnonymizedColumnsMixin = type('AnonymizedColumnsMixin', (), {
+        column.name: db.Column(ColumnType(column.type, key, engine))
+        for column in model.__table__.columns
+        if column.info.get('anonymize')
+    })
+
+    class CustomerPersonalInfo(AnonymizedColumnsMixin, db.Model):
+        """
+        Class writes deleted customer's personal info encrypted
+        on the isolated database
+        """
+        __bind_key__ = 'db_isolated'
+        __tablename__ = 'customers_personal_info'
+
+        customerid = db.Column('customerid', db.Integer, primary_key=True)
+
+        @classmethod
+        def from_customer(cls, customer):
+            self = cls()
+            for column in self.__table__.columns:
+                setattr(self, column.name, getattr(customer, column.name))
+            return self
+
+    return CustomerPersonalInfo
+
+
+def _get_key():
+    session['cryptkey'] = session.get('cryptkey', Fernet.generate_key())
+    return session['cryptkey']
+
+CustomerPersonalInfo = _get_customer_personal_info(
+    Customers, EncryptedType, FernetEngine, _get_key)
