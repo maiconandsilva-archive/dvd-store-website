c93a9d2cfc3cede7b7f17a47d833c309e48a9c18
pablo.barbosa <pablo.barbosa@anovasistemas.com.br>
2020-11-24 10:00:30 -0300

    finalizada as rotas de erro 404 e 500 #9

--- views/http_errors.py
+++ views/http_errors.py
@@ -2,7 +2,13 @@ from flask import render_template
 
 from app import app
 
+FILE_PATH_DEFAULT = 'img/general_error.svg'
 
 @app.errorhandler(404)
 def erro_404(error):
-    return render_template('404.jinja')
+    file_name = 'img/error_404.svg'
+    return render_template('http_errors/404.jinja')
+
+@app.errorhandler(500)
+def erro_500(error):
+    return render_template('http_error/500.jinja')
