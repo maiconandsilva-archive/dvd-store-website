0132d0e189d3042c1dc4eb52a1ebab0dd04ab4b2
gabriel.moura <gabriel.moura@gsw.com.br>
2020-11-28 21:50:26 -0300

    :art: Adição das rotas para os dados pessoais, endereços e pagementos

--- server.py
+++ server.py
@@ -8,6 +8,9 @@ from views import *
 app.add_url_rule('/signin', view_func=Signin.as_view('signin'))
 app.add_url_rule('/signup', view_func=Signup.as_view('signup'))
 app.add_url_rule('/account', view_func=AccountView.as_view('account'))
+app.add_url_rule('/account/personal-info', view_func=AccountPersonalInfoView.as_view('personal-info'))
+app.add_url_rule('/account/address-info', view_func=AccountAddressInfoView.as_view('address-info'))
+app.add_url_rule('/account/payments-info', view_func=AccountPaymentsInfoView.as_view('payments-info'))
 app.add_url_rule('/account/signout', view_func=Signout.as_view('signout'))
 app.add_url_rule('/account/delete', view_func=AccountDelete.as_view('account-delete'))
 
